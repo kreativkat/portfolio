36cce0b1af6d4ccca32c72b73c79b73ebcc1c66e
marlonG <maruronu@gmail.com>
2024-12-18 02:00:08 +0800

    partnered brands done

--- src/app/components/home/PartneredBrands.jsx
+++ src/app/components/home/PartneredBrands.jsx
@@ -1,78 +1,9 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import { brandsRow1, brandsRow2 } from "./brandsData";
 
 const PartneredBrands = () => {
-  const brandsRow1 = [
-    {
-      name: "Creators Value",
-      logo: "/brands/creators-value.png",
-    },
-    {
-      name: "Feat Books",
-      logo: "/brands/featbooks.png",
-    },
-    {
-      name: "Spark Up",
-      logo: "/brands/SparkUp.png",
-    },
-    {
-      name: "Feat Radio",
-      logo: "/brands/featradio.png",
-    },
-    {
-      name: "WellTalk",
-      logo: "/brands/WellTalk.png",
-    },
-    {
-      name: "Kennet Cobonpue",
-      logo: "/brands/kennetcobonpue.jpg",
-    },
-    {
-      name: "Pacific Traders",
-      logo: "/brands/pacificTraders.png",
-    },
-    {
-      name: "Plan Out",
-      logo: "/brands/planout.jpg",
-    },
-    {
-      name: "Winnia",
-      logo: "/brands/winnia.jpg",
-    },
-  ];
-
-  const brandsRow2 = [
-    {
-      name: "ASBIR",
-      logo: "/brands/asbir.png",
-    },
-    {
-      name: "Aim Radio",
-      logo: "/brands/aimRadio.png",
-    },
-    {
-      name: "Up Radio",
-      logo: "/brands/UpRadio.png",
-    },
-    {
-      name: "Beetzee",
-      logo: "/brands/beetzee.jpg",
-    },
-    {
-      name: "nextART",
-      logo: "/brands/nextART.jpg",
-    },
-    {
-      name: "Paraiso",
-      logo: "/brands/paraiso.jpg",
-    },
-    {
-      name: "Romoza",
-      logo: "/brands/romoza.jpg",
-    },
-  ];
-
   return (
     <section className="w-full py-16 bg-secondary font-hostGrotesk overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
---
+++ src/app/components/home/brandsData.js
@@ -0,0 +1,69 @@
+export const brandsRow1 = [
+  {
+    name: "Creators Value",
+    logo: "/brands/creators-value.png",
+  },
+  {
+    name: "Feat Books",
+    logo: "/brands/featbooks.png",
+  },
+  {
+    name: "Spark Up",
+    logo: "/brands/SparkUp.png",
+  },
+  {
+    name: "Feat Radio",
+    logo: "/brands/featradio.png",
+  },
+  {
+    name: "WellTalk",
+    logo: "/brands/WellTalk.png",
+  },
+  {
+    name: "Kennet Cobonpue",
+    logo: "/brands/kennetcobonpue.jpg",
+  },
+  {
+    name: "Pacific Traders",
+    logo: "/brands/pacificTraders.png",
+  },
+  {
+    name: "Plan Out",
+    logo: "/brands/planout.jpg",
+  },
+  {
+    name: "Winnia",
+    logo: "/brands/winnia.jpg",
+  },
+];
+
+export const brandsRow2 = [
+  {
+    name: "ASBIR",
+    logo: "/brands/asbir.png",
+  },
+  {
+    name: "Aim Radio",
+    logo: "/brands/aimRadio.png",
+  },
+  {
+    name: "Up Radio",
+    logo: "/brands/UpRadio.png",
+  },
+  {
+    name: "Beetzee",
+    logo: "/brands/beetzee.jpg",
+  },
+  {
+    name: "nextART",
+    logo: "/brands/nextART.jpg",
+  },
+  {
+    name: "Paraiso",
+    logo: "/brands/paraiso.jpg",
+  },
+  {
+    name: "Romoza",
+    logo: "/brands/romoza.jpg",
+  },
+];
